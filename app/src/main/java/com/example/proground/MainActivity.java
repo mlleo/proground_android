6cbb525217cd7f0003828c24d6b0150f8e4aaa2a
jeongho <0x01.leo@gmail.com>
2020-10-09 17:36:52 +0900

    registration & login logic done

--- app/src/main/java/com/example/proground/MainActivity.java
+++ app/src/main/java/com/example/proground/MainActivity.java
@@ -3,28 +3,66 @@ package com.example.proground;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button main_rgt_btn;
+    Button main_log_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //start point
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        main_rgt_btn = findViewById(R.id.main_btn_register);
+        main_log_btn = findViewById(R.id.main_btn_login);
+
+        main_rgt_btn.setOnClickListener(onClickListener);
+        main_log_btn.setOnClickListener(onClickListener);
     }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()){
-            case android.R.id.home:{
-                finish();
-                return true;
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.main_btn_register:
+                    main2RegisterActivity();
+                    break;
+                case R.id.main_btn_login:
+                    main2LoginActivity();
+                    break;
+
             }
         }
-        return super.onOptionsItemSelected(item);
+    };
+
+    private void main2LoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
+
+    private void main2RegisterActivity(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        switch (item.getItemId()){
+//            case android.R.id.home:{
+//                finish();
+//                return true;
+//            }
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
--- app/src/main/java/com/example/proground/RegisterActivity.java
+++ app/src/main/java/com/example/proground/RegisterActivity.java
@@ -1,10 +1,12 @@
 package com.example.proground;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -25,6 +27,7 @@ public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     EditText memail, mpassword, mpasswordcheck, mnickname, mbirth, msex, mheight, mweight;
     Button rgtBtn;
+    TextView rgt_to_log_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //start point
@@ -42,8 +45,11 @@ public class RegisterActivity extends AppCompatActivity {
         mheight = findViewById(R.id.rgt_height);
         mweight = findViewById(R.id.rgt_weight);
         rgtBtn = findViewById(R.id.rgt_btn);
+        rgt_to_log_btn = (TextView)findViewById(R.id.rgt_to_lgn_btn); //textview
 
         rgtBtn.setOnClickListener(onClickListener);
+        rgt_to_log_btn.setOnClickListener(onClickListener);
+
 
     }
 
@@ -61,7 +67,10 @@ public class RegisterActivity extends AppCompatActivity {
             switch (v.getId()) {
                 case R.id.rgt_btn:
                     signUp();
+                    startLoginActivity();
                     break;
+                case R.id.rgt_to_lgn_btn:
+                    startLoginActivity();
 
             }
         }
@@ -140,5 +149,10 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void startToast(String msg){
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void startLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
